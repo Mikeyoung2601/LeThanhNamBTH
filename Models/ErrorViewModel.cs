1ed6ececbfba88b04ef8c830871c04ac71e61f3d
Nam Good Kid <hoat6704@gmail.com>
2022-09-15 14:52:19 +0700

    LTN_1409

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LeThanhNamBTH.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Models/Student.cs
@@ -0,0 +1,13 @@
+namespace LeThanhNamBTH.Models
+{
+    public class Student
+    {
+        //khai bao 3 thuoc tinh cua doi tuong
+        public int StudentID {get; set;}
+
+        public string? StudentName {get; set;}
+
+        public int Age {get; set;}
+      
+    }
+}
